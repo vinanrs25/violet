378311d2012a42841706df64beca61f18419c97d
vinanrs25 <vinanurnisa225@gmail.com>
2025-01-02 17:47:24 +0700

    Update suna.py

--- suna.py
+++ suna.py
@@ -1,1 +1,9 @@
 print("Hello Suna")
+def suna():
+    data = input("Masukkan nama anda: ")
+    if data == "Vina".lower():
+      print("Hai vina kamu cewe aku (suna)")
+    else:
+      print("Siapa sih kamu, ak ga kenal ga peduli y.")
+
+suna()
