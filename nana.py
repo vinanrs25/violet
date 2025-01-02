df4d6fbe8b81ba2293f973f77c9a2e604c7392fe
vinanrs25 <vinanurnisa225@gmail.com>
2025-01-02 17:36:55 +0700

    gabut azza

--- nana.py
+++ nana.py
@@ -2,3 +2,21 @@ print("Hello World")
 print("Hello World")
 print("Hello World")
 print("AKU VINA")
+
+def vina():
+    while True:
+        data = input("Siapa nama kamu?")
+        if data == "Vina".lower():
+            print("Halo Vina")
+            continue
+        elif data == "Violet".lower():
+            print("Halo Violet")
+            continue
+        elif data == "Suna".lower():
+            print("Halo Suna")
+            continue
+        else:
+            print("Maaf, nama kamu tidak dikenal")
+            break
+
+vina()
